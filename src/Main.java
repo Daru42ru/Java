0add296cb98ee6ab53e1c17ecd2686ddfc42bad4
Дарья Кротова <daru42ru@gmail.com>
2023-03-09 14:01:15 +0700

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {   int cost = 14562; // стоимость билета
+        int bonus = 20; // стоимость 1 мили
+        int mili = cost / bonus; // количество начисляемых миль
+        System.out.println("Вам начислено:");
+        System.out.println(mili + " бонусных миль");
     }
 }
